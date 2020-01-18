1de3fcc81f637bb7e289f04ad17bdc3246197627
rokerzfirst101 <33196098+rokerzfirst101@users.noreply.github.com>
2020-01-18 10:34:10 +0530

    Web App added.

---
+++ public/dashboard.js
@@ -0,0 +1,25 @@
+function initApp() {
+    // Listening for auth state changes.
+    // [START authstatelistener]
+    firebase.auth().onAuthStateChanged(function(user) {
+      // [START_EXCLUDE silent]
+      // [END_EXCLUDE]
+      if (user) {
+        // User is signed in.
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        var providerData = user.providerData;
+        // [START_EXCLUDE]
+        document.getElementById('account-details').textContent = JSON.stringify(user, null, '  ');
+        // [END_EXCLUDE]
+      } 
+    });
+    }
+
+  window.onload = function() {
+    initApp();
+  };
